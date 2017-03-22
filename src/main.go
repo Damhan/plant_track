c2dde982e45f35e496a2185f7a999a14e0d43415
Damhan Richardson <damhanrichardson@Gmail.com>
2017-03-22 16:22:10 +0000

    Added ability to show plants age in days
    Now dynamically shows plants age on viewing.

--- src/main.go
+++ src/main.go
@@ -9,8 +9,6 @@ import (
   _ "github.com/mattn/go-sqlite3"
   )
 
-
-
 //also simple input/trim function for reading input and stripping newlines
 func readTrim(r *bufio.Reader) string {
   line, _ := r.ReadString('\n')
@@ -25,7 +23,6 @@ func checkErr(err error) {
   }
 }
 
-
 //creates database if it doesn't exist and connects to it.
 func createConnect() *sql.DB {
   db, err := sql.Open("sqlite3", "./plants.db")
@@ -35,45 +32,41 @@ func createConnect() *sql.DB {
 
 //function that creates required tables if they don't exist
 func createTables(db *sql.DB) {
-  _, err := db.Exec("CREATE TABLE IF NOT EXISTS vegPlants(ID TEXT, Plant TEXT, Age INTEGER)")
+  _, err := db.Exec("CREATE TABLE IF NOT EXISTS vegPlants(ID TEXT, Plant TEXT,PlantDate TEXT)")
   checkErr(err)
 }
 
 //function that handles inserting values into a dynamic table of choice from main.
 func table_insertion(db *sql.DB, table string, insert_info string) {
-
   var plant_info = strings.Split(insert_info,"/") //split our info to be inserted into an array
-  stmt, err:= db.Prepare("INSERT INTO vegPlants(ID,Plant,Age) VALUES(?,?,?)")
+  stmt, err:= db.Prepare("INSERT INTO vegPlants(ID,Plant,PlantDate) VALUES(?,?,?)")
   checkErr(err)
   _,err = stmt.Exec(plant_info[0],plant_info[1],plant_info[2])
   checkErr(err)
 }
 
-
 func tableViewing(db *sql.DB,table_choice string) {
-  rows, err := db.Query("SELECT * FROM vegPlants")
+  rows, err := db.Query("SELECT ID,Plant,PlantDate,Cast((JulianDay()-JulianDay(PlantDate)) AS INT) FROM vegPlants")
   checkErr(err)
 
   var ID string
   var Plant string
-  var Age int
+  var PlantDate string
+  var Age string
 
-  fmt.Println("Plant ID | Plant Name | Plant Veg Age")
+  fmt.Println("ID|PlantName|PlantDate|PlantAge")
 
   for rows.Next() {
-    err = rows.Scan(&ID,&Plant,&Age)
+    err = rows.Scan(&ID,&Plant,&PlantDate,&Age)
     checkErr(err)
 
   fmt.Print(ID + " | ")
   fmt.Print(Plant + " | ")
+  fmt.Print(PlantDate + " | ")
   fmt.Print(Age)
   fmt.Println(" ")
   }
 }
-
-
-
-
 //main
 func main() {
   //setup
@@ -92,7 +85,8 @@ func main() {
     if strings.Compare(choice,"i") == 0{
       fmt.Print("What table would you like to insert to? ")
       table_choice := readTrim(reader)
-      fmt.Println("Please insert in the form; 'id/plant/age' followed by an enter ")
+      fmt.Println("Please insert in the form; 'id/plant/plantdate' followed by an enter ")
+      fmt.Println("Please make sure the plant date is in the form YYYY-MM-DD")
       insert_info := readTrim(reader)
       table_insertion(db,table_choice,insert_info)
     }
